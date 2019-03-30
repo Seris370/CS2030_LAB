e6f1d263016bdec1d1b780affb074437e646d84c
Seris370 <38371667+Seris370@users.noreply.github.com>
2019-03-31 00:49:09 +0800

    Update level_2.java

--- Rubik's Cube/level_2.java
+++ Rubik's Cube/level_2.java
@@ -1,3 +1,80 @@
+public class Face {
+    private int[][] grid;
+
+    public Face(int[][] _grid) {
+      int[][] grid = {{1,2,3},{4,5,6},{7,8,9}};
+      grid[0] = _grid[0].clone();
+      grid[1] = _grid[1].clone();
+      grid[2] = _grid[2].clone();
+      this.grid = grid;
+    }
+
+    public Face rotateRight() {
+        int newGrid[][] = new int[3][3];
+        int[][] grid = {{1,2,3},{4,5,6},{7,8,9}};
+        grid[0] = this.grid[0].clone();
+        grid[1] = this.grid[1].clone();
+        grid[2] = this.grid[2].clone();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                newGrid[i][j] = grid[2 - j][i];
+            }
+        }
+        return new Face(newGrid);
+    }
+
+    public Face rotateLeft() {
+        int[][] newGrid = new int[3][3];
+        int[][] grid = {{1,2,3},{4,5,6},{7,8,9}};
+        grid[0] = this.grid[0].clone();
+        grid[1] = this.grid[1].clone();
+        grid[2] = this.grid[2].clone();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                newGrid[2 - j][i] = grid[i][j];
+            }
+        }
+        return new Face(newGrid);
+    }
+
+    public Face rotateHalf() {
+        return this.rotateLeft().rotateLeft();
+    }
+
+    public int[][] getGrid() {
+        return this.grid;
+    }
+
+    public String toString() {
+        int r = grid.length;
+        String str = "";
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < r; j++) {
+                str += String.format("%02d", grid[i][j]);
+                if (j == r - 1) {
+                    str += String.format("\n");
+                }
+            }
+        }
+        return str;
+    }
+
+    public String pR() {
+        int r = grid.length;
+        String str = "";
+        String dot = "......";
+        for (int i = 0; i < r; i++) {
+            str += dot;
+            for (int j = 0; j < r; j++) {
+                str += String.format("%02d", grid[i][j]);
+            }
+            str += dot;
+            str += "\n";
+        }
+        return str;
+    }
+}
+
 public class Rubik {
     private int[][][] grid;
     private int r;
